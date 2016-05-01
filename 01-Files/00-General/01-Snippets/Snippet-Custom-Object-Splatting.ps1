c6d9e0d7709243510cbed9f69a2a4bd3ed065e83
Richard Diphoorn <rdiphoorn@outlook.com>
2016-05-01 03:09:14 +0200

    Update

---
+++ 01-Files/00-General/01-Snippets/Snippet-Custom-Object-Splatting.ps1
@@ -0,0 +1,25 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.120
+	 Created on:   	2016-05-01 3:04 AM
+	 Created by:   	Richard Diphoorn
+	 Organization: 	
+	 Filename:     	Snippet-Custom-Object-Splatting
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+$customObject = [ordered]@{
+	Name = '<name>'
+	Job = '<jobname>'
+	Company = '<companyname>'
+	EMail = '<mailaddress>'
+	BloggerAt = 'http://<url>'
+	ModeratorAt = 'http://<url>'
+	Twitter = 'http://twitter.com/<username>'
+	GitHub = 'https://github.com/<username>'
+}
+
+New-Object -TypeName PSObject -ArgumentList $customObject

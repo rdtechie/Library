c03e39928a94762d279d43f2bdc1f71f80f55c11
Richard Diphoorn <rdiphoorn@outlook.com>
2016-05-04 11:09:05 +0200

    Update

---
+++ 01-Files/02-VMware/01-Inventory/Function-Get-VMwareVCPUtoThreadRatio.ps1
@@ -0,0 +1,49 @@
+﻿function Get-VMwareVCPUtoThreadRatio {
+	[CmdletBinding()]
+	param (
+		[Parameter(Position = 0, Mandatory = $false)]
+		[System.String]$Hosts = '*'
+	)
+	begin {
+		try {
+		}
+		catch {
+		}
+	}
+	process {
+		try {
+			$vmHosts = Get-VMHost $vmHosts
+			$vms = Get-VM
+		}
+		catch {
+			$_
+		}
+		
+		try {
+			ForEach ($vmHost in $vmHosts) {
+				
+				$vCPUs = 0
+				$ratio = $null
+				$hostThreads = $vmhost.extensiondata.hardware.cpuinfo.numcputhreads
+				$vms | Where-Object { $_.vmhost -like $vmHost } | ForEach-Object { $vCPUs += $_.numcpu }
+				if ($vCPUs -ne '0') { $ratio = "$("{0:N2}" -f ($vCPUs/$hostThreads))" + ":1" }
+				
+				[pscustomobject][Ordered]@{
+					Hostname = $vmHost.Name
+					PhysicalThreads = $hostThreads
+					vCPUs = $vCPUs
+					Ratio = $ratio
+				}
+			}
+		}
+		catch {
+			$_
+		}
+	}
+	end {
+		try {
+		}
+		catch {
+		}
+	}
+}

aa79d7745e1a44f9bd1427c7a4ce2d4d55095f03
Richard Diphoorn <rdiphoorn@outlook.com>
2016-05-04 15:16:16 +0200

    Update

---
+++ 01-Files/02-VMware/01-Inventory/Function-Get-VMwareVMThinProvisionedVMDK.ps1
@@ -0,0 +1,45 @@
+﻿function Get-VMwareVMThinProvisionedVMDK {
+	[CmdletBinding()]
+	param (
+		[Parameter(Position = 0, Mandatory = $true)]
+		[System.String[]]$VMname = '*'
+	)
+	begin {
+		try {
+		}
+		catch {
+		}
+	}
+	process {
+		try {
+			$vms = Get-VM $VMname
+		}
+		catch {
+			$_
+		}
+		try {
+			ForEach ($vm in $vms) {
+				$view = Get-View $vm
+				if ($view.config.hardware.Device.Backing.ThinProvisioned -eq $true) {
+					
+					[pscustomobject][Ordered]@{
+						Name = $vm.Name
+						Provisioned = [math]::round($vm.ProvisionedSpaceGB, 2)
+						Total = [math]::round(($view.config.hardware.Device | Measure-Object CapacityInKB -Sum).sum/1048576, 2)
+						Used = [math]::round($vm.UsedSpaceGB, 2)
+						VMDKs = $view.config.hardware.Device.Backing.Filename | Out-String
+						Thin = $view.config.hardware.Device.Backing.ThinProvisioned | Out-String
+					}
+				}
+			}
+		}
+		catch {
+		}
+	}
+	end {
+		try {
+		}
+		catch {
+		}
+	}
+}
